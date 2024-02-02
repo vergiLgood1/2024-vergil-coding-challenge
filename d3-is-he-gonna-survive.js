6aad945b4b773447aae2f57ab884300df93745b0
Diyo Anggara <you@example.com git config --global user.email>
2024-02-02 21:50:02 +0700

    day 3/366

---
+++ d3-is-he-gonna-survive.js
@@ -0,0 +1,22 @@
+// vergil challenge 2024
+// 3/366
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/
+
+function hero(bullets, dragons){
+    //Get Coding!
+    if(dragons * 2 <= bullets){
+        return true
+    }else{
+        return false
+    }
+}
+
+// function hero(bullets, dragons){
+
+//     return bullets >= dragons * 2 
+
+// }
+
+// const hero = (bullets, dragons) => bullets >= dragons * 2
+
+ // console.log(hero(25, 5))
